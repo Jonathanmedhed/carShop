4641ca3621400fde83d6e5493e7e81d3d1406fad
Jonathan Medina <jonathanmedhed@gmail.com>
2019-10-19 23:59:35 +0100

    navbar finctionality

--- carShop/static/js/main.js
+++ carShop/static/js/main.js
@@ -4,3 +4,19 @@ document.querySelector('.year').innerHTML = date.getFullYear();
 setTimeout(function () {
     $('#message').fadeOut('slow')
 }, 3000);
+
+$(document).ready(function () {
+    $('#mycarousel').carousel({ interval: 2000 });
+    $('#carouselButton').click(function () {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+});
