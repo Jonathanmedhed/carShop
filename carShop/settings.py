90b3b1ccd13b3be49773a30d0087ed0a4411b1f0
Jonathan Medina <jonathanmedhed@gmail.com>
2019-10-23 20:15:57 +0100

    Listing modal created

--- carShop/settings.py
+++ carShop/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'carShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carshop',
+        'USER':'postgres',
+        'PASSWORD':'pass',
+        'HOST': 'localhost'
     }
 }
 
--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Listing(models.Model):
+    # Delete listing if user is deleted
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.CharField(max_length=4)
+    millage = models.CharField(max_length=100)
+    colour = models.CharField(max_length=100)
+    body = models.TextField(max_length=100)
+    city = models.TextField(max_length=100)
+    price = models.IntegerField()
+    cc = models.IntegerField(blank=True)
+    description = models.TextField(blank=True)  # can be empty
+    # upload to media folder with date
+    photo_front = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_back = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_left = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_right = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_extra = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_extra2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):  # Main field to display
+        return self.model
