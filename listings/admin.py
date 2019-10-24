b84fae59236bb143dd0b3185093bd98249000d06
Jonathan Medina <jonathanmedhed@gmail.com>
2019-10-24 03:22:33 +0100

    search car half done

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    #to displayinfo
+    list_display = ('id', 'make', 'model', 'is_published', 'price', 'list_date', 'user') 
+    #to make them clickable
+    list_display_links = ('id', 'make', 'model') 
+    #To add filters
+    list_filter = ('make', 'model', 'is_published', 'price', 'list_date', 'user') 
+    #Make a field editable
+    list_editable= ('is_published',)
+    #Add search field
+    search_fields = ('make', 'model', 'is_published', 'price', 'list_date', 'user', 'city')
+    #amount per page
+    list_per_page= 25
+
+admin.site.register(Listing, ListingAdmin)
--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,87 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Listing
+from .choices import car_make_choices, moto_make_choices
+
 
 def car(request):
-    return render(request, 'listings/car.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/car.html', context)
+
 
 def moto(request):
-    return render(request, 'listings/moto.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/moto.html', context)
+
+'''
+def search_car(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Make
+    if 'make' in request.GET:
+        make = request.GET['make']
+        if make:
+            queryset_list = queryset_list.filter(
+                make__iexact=make)
+
+    # Model
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            queryset_list = queryset_list.filter(
+                model__iexact=model)
+
+    #Body
+    if 'body' in request.GET:
+        body = request.GET['body']
+        if body:
+            queryset_list = queryset_list.filter(
+                body__iexact=body)
+
+    #Colour
+    if 'colour' in request.GET:
+        colour = request.GET['colour']
+        if colour:
+            queryset_list = queryset_list.filter(
+                colour__iexact=colour)
+
+    context = {
+        'car_make_choices': car_make_choices,
+        'moto_make_choices': moto_make_choices,
+        'listings': queryset_list,
+        'values': request.GET #to mantain search values in the fields after search
+    }
+    return render(request, 'listings/search_car.html', context)
+
+'''
+def search_moto(request):
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/search_moto.html', context)
 
 def search_car(request):
-    return render(request, 'listings/search_car.html')
+    
+    queryset_list = Listing.objects.order_by('-list_date')
+    
+    # State
+    if 'make' in request.GET:
+        make = request.GET['make']
+        if make:
+            queryset_list = queryset_list.filter(
+                make__iexact=make)
 
-def search_moto(request):
-    return render(request, 'listings/search_moto.html')
+    context = {
+        'car_make_choices': car_make_choices,
+        'moto_make_choices': moto_make_choices,
+        'listings': queryset_list,
+        'values': request.GET #to mantain search values in the fields after search
+    }
+
+    return render(request, 'listings/search_car.html', context)
